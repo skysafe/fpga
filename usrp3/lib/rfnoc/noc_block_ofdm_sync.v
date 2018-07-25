12734c7299cb37e031e9aa411e0870f8b01ac429
Jonathon Pendlum <jonathon@pendlumtech.com>
2018-07-26 02:56:07 +0900

    lib: rfnoc: Initial commit noc block ofdm sync -- WIP

---
+++ usrp3/lib/rfnoc/noc_block_ofdm_sync.v
@@ -0,0 +1,141 @@
+//
+// Copyright 2018 SkySafe Inc.
+//
+
+module noc_block_ofdm_sync #(
+  parameter NOC_ID = 64'h0FD3_0001_0000_0000,
+  parameter STR_SINK_FIFOSIZE = 11)
+(
+  input bus_clk, input bus_rst,
+  input ce_clk, input ce_rst,
+  input  [63:0] i_tdata, input  i_tlast, input  i_tvalid, output i_tready,
+  output [63:0] o_tdata, output o_tlast, output o_tvalid, input  o_tready,
+  output [63:0] debug
+);
+
+  ////////////////////////////////////////////////////////////
+  //
+  // RFNoC Shell
+  //
+  ////////////////////////////////////////////////////////////
+  wire [31:0] set_data;
+  wire [7:0]  set_addr;
+  wire        set_stb;
+
+  wire [63:0] str_sink_tdata, str_src_tdata;
+  wire        str_sink_tlast, str_sink_tvalid, str_sink_tready, str_src_tlast, str_src_tvalid, str_src_tready;
+
+  wire        clear_tx_seqnum;
+  wire [15:0] src_sid, next_dst_sid;
+
+  noc_shell #(
+    .NOC_ID(NOC_ID),
+    .STR_SINK_FIFOSIZE(STR_SINK_FIFOSIZE))
+  noc_shell (
+    .bus_clk(bus_clk), .bus_rst(bus_rst),
+    .i_tdata(i_tdata), .i_tlast(i_tlast), .i_tvalid(i_tvalid), .i_tready(i_tready),
+    .o_tdata(o_tdata), .o_tlast(o_tlast), .o_tvalid(o_tvalid), .o_tready(o_tready),
+    // Computer Engine Clock Domain
+    .clk(ce_clk), .reset(ce_rst),
+    // Control Sink
+    .set_data(set_data), .set_addr(set_addr), .set_stb(set_stb), .set_time(), .set_has_time(),
+    .rb_stb(1'b1), .rb_data(64'h0), .rb_addr(),
+    // Control Source
+    .cmdout_tdata(64'h0), .cmdout_tlast(1'b0), .cmdout_tvalid(1'b0), .cmdout_tready(),
+    .ackin_tdata(), .ackin_tlast(), .ackin_tvalid(), .ackin_tready(1'b1),
+    // Stream Sink
+    .str_sink_tdata(str_sink_tdata), .str_sink_tlast(str_sink_tlast), .str_sink_tvalid(str_sink_tvalid), .str_sink_tready(str_sink_tready),
+    // Stream Source
+    .str_src_tdata(str_src_tdata), .str_src_tlast(str_src_tlast), .str_src_tvalid(str_src_tvalid), .str_src_tready(str_src_tready),
+    // Misc
+    .vita_time(64'd0), .clear_tx_seqnum(clear_tx_seqnum),
+    .src_sid(src_sid), .next_dst_sid(next_dst_sid), .resp_in_dst_sid(), .resp_out_dst_sid(),
+    .debug(debug));
+
+  ////////////////////////////////////////////////////////////
+  //
+  // AXI Wrapper
+  // Convert RFNoC Shell interface into AXI stream interface
+  //
+  ////////////////////////////////////////////////////////////
+  wire [31:0]  m_axis_data_tdata;
+  wire         m_axis_data_tlast;
+  wire         m_axis_data_tvalid;
+  wire         m_axis_data_tready;
+
+  wire [31:0]  s_axis_data_tdata;
+  wire         s_axis_data_tlast;
+  wire         s_axis_data_tvalid;
+  wire         s_axis_data_tready;
+  
+  wire sof;
+
+  axi_wrapper #(
+    .SIMPLE_MODE(0))
+  axi_wrapper (
+    .clk(ce_clk), .reset(ce_rst),
+    .bus_clk(bus_clk), .bus_rst(bus_rst),
+    .clear_tx_seqnum(clear_tx_seqnum),
+    .next_dst(next_dst_sid),
+    .set_stb(set_stb), .set_addr(set_addr), .set_data(set_data),
+    .i_tdata(str_sink_tdata), .i_tlast(str_sink_tlast), .i_tvalid(str_sink_tvalid), .i_tready(str_sink_tready),
+    .o_tdata(str_src_tdata), .o_tlast(str_src_tlast), .o_tvalid(str_src_tvalid), .o_tready(str_src_tready),
+    .m_axis_data_tdata(m_axis_data_tdata),
+    .m_axis_data_tlast(m_axis_data_tlast),
+    .m_axis_data_tvalid(m_axis_data_tvalid),
+    .m_axis_data_tready(m_axis_data_tready),
+    .m_axis_data_tuser(),
+    .s_axis_data_tdata(s_axis_data_tdata),
+    .s_axis_data_tlast(s_axis_data_tlast),
+    .s_axis_data_tvalid(s_axis_data_tvalid),
+    .s_axis_data_tready(s_axis_data_tready),
+    // Packet type, sequence number, and length will be automatically filled
+    // Using EOB bit to indicate start of frame
+    .s_axis_data_tuser({2'd0,1'd0,sof,12'd0,16'd0,{src_sid,next_dst_sid},64'd0}),
+    // Unused
+    .m_axis_config_tdata(),
+    .m_axis_config_tlast(),
+    .m_axis_config_tvalid(),
+    .m_axis_config_tready(),
+    .m_axis_pkt_len_tdata(),
+    .m_axis_pkt_len_tvalid(),
+    .m_axis_pkt_len_tready());
+  
+  ////////////////////////////////////////////////////////////
+  //
+  // User code
+  //
+  ////////////////////////////////////////////////////////////
+  localparam [7:0] BASE = 129;
+
+  // Quantized long preamble
+  localparam [32*96-1:0] LONG_PREAMBLE_XCORR_QUANT = {
+    {-1,-1},{ 1,-1},{ 1, 1},{ 1, 1},{ 1,-1},{-1,-1},{-1,-1},{ 1,-1},
+    { 1, 1},{ 1,-1},{-1,-1},{ 1,-1},{ 1,-1},{-1, 1},{ 1,-1},{ 1,-1},
+    { 1, 1},{-1, 1},{-1, 1},{ 1, 1},{ 1, 1},{-1, 1},{-1,-1},{-1,-1},
+    {-1,-1},{-1,-1},{ 1,-1},{-1, 1},{-1, 1},{ 1, 1},{ 1, 1},{-1,-1},
+    { 1,-1},{ 1,-1},{-1,-1},{-1,-1},{ 1, 1},{-1, 1},{-1, 1},{-1, 1},
+    {-1, 1},{-1,-1},{ 1,-1},{ 1,-1},{-1,-1},{-1,-1},{ 1,-1},{ 1, 1},
+    { 1, 1},{-1,-1},{ 1, 1},{ 1, 1},{-1, 1},{ 1, 1},{ 1,-1},{ 1, 1},
+    {-1, 1},{-1, 1},{ 1, 1},{ 1,-1},{ 1,-1},{ 1, 1},{-1, 1},{ 1,-1},
+    {-1,-1},{ 1,-1},{ 1, 1},{ 1, 1},{ 1,-1},{-1,-1},{-1,-1},{ 1,-1},
+    { 1, 1},{ 1,-1},{-1,-1},{ 1,-1},{ 1,-1},{-1, 1},{ 1,-1},{ 1,-1},
+    { 1, 1},{-1, 1},{-1, 1},{ 1, 1},{ 1, 1},{-1, 1},{-1,-1},{-1,-1},
+    {-1,-1},{-1,-1},{ 1,-1},{-1, 1},{-1, 1},{ 1, 1},{ 1, 1},{-1,-1}
+  }
+
+  ofdm_sync #(
+    .WINDOW_LEN(64),
+    .SYMBOL_LEN(64),
+    .CYCLIC_PREFIX_LEN(16),
+    .SHORT_PREAMBLE_LEN(160),
+    .LONG_PREAMBLE_XCORR_LEN(80),
+    .LONG_PREAMBLE_XCORR_QUANT(LONG_PREAMBLE_XCORR_QUANT)
+  ) inst_ofdm_sync (
+    .clk(ce_clk), .reset(ce_rst),
+    .i_tdata(m_axis_data_tdata), .i_tlast(m_axis_data_tlast), .i_tvalid(m_axis_data_tvalid), .i_tready(m_axis_data_tready),
+    .o_tdata(s_axis_data_tdata), .o_tlast(s_axis_data_tlast), .o_tvalid(s_axis_data_tvalid), .o_tready(s_axis_data_tready),
+    .num_symbols(), .num_symbols_valid(1'b0),
+    .sof(sof), .eof());
+
+endmodule
---
+++ usrp3/lib/rfnoc/ofdm_sync.v
@@ -0,0 +1,220 @@
+//
+// Copyright 2018 SkySafe Inc.
+//
+module ofdm_sync #(
+  parameter WINDOW_LEN = 64,
+  parameter SYMBOL_LEN = 64,
+  parameter CYCLIC_PREFIX_LEN  = 16,
+  parameter SHORT_PREAMBLE_LEN = 160,
+  parameter LONG_PREAMBLE_XCORR_LEN = 80,
+  parameter [32*2*LONG_PREAMBLE_XCORR_LEN-1:0] LONG_PREAMBLE_XCORR_QUANT = 0
+)(
+  input clk, input reset, input clear,
+  input set_stb, input [7:0] set_addr, input [31:0] set_data,
+  input [31:0] i_tdata, input i_tlast, input i_tvalid, output i_tready,
+  output [31:0] o_tdata, output o_tlast, output o_tvalid, input o_tready,
+  input [7:0] num_symbols, input num_symbols_valid,
+  output sof,  // Start of frame at long preamble
+  output eof   // End of frame
+);
+
+  wire [31:0] samples_in_tdata[0:3];
+  wire [3:0] samples_in_tvalid, samples_in_tready;
+  split_stream_fifo #(
+    .WIDTH(32),
+    .ACTIVE_MASK(4'b1111),
+    .FIFO_SIZE(0))
+  inst_split_stream_fifo_samples (
+    .clk(clk), .reset(reset), .clear(1'b0),
+    .i_tdata(i_tdata), .i_tlast(1'b0), .i_tvalid(i_tvalid), .i_tready(i_tready),
+    .o0_tdata(samples_in_tdata[0]), .o0_tlast(), .o0_tvalid(samples_in_tvalid[0]), .o0_tready(samples_in_tready[0]),
+    .o1_tdata(samples_in_tdata[1]), .o1_tlast(), .o1_tvalid(samples_in_tvalid[1]), .o1_tready(samples_in_tready[1]),
+    .o2_tdata(samples_in_tdata[2]), .o2_tlast(), .o2_tvalid(samples_in_tvalid[2]), .o2_tready(samples_in_tready[2]),
+    .o3_tdata(samples_in_tdata[3]), .o3_tlast(), .o3_tvalid(samples_in_tvalid[3]), .o2_tready(samples_in_tready[3]));
+
+  /////////////////////////////////////////////////////////
+  // Calculate correlation, P(d)
+  /////////////////////////////////////////////////////////
+  wire [31:0] samples_dly_tdata;
+  wire        samples_dly_tvalid, samples_dly_tready;
+  delay_fifo #(
+    .MAX_LEN(WINDOW_LEN),
+    .WIDTH(32))
+  inst_delay_fifo_samples (
+    .clk(clk), .reset(reset), .clear(1'b0),
+    .len(WINDOW_LEN),
+    .i_tdata(samples_in_tdata[0]), .i_tlast(1'b0), .i_tvalid(samples_in_tvalid[0]), .i_tready(samples_in_tready[0]),
+    .o_tdata(samples_dly_tdata), .o_tlast(), .o_tvalid(samples_dly_tvalid), .o_tready(samples_dly_tready));
+
+  wire [31:0] samples_conj_tdata;
+  wire        samples_conj_tvalid, samples_conj_tready;
+  conj #(
+    .WIDTH(16))
+  inst_conj_samples (
+    .clk(clk), .reset(reset), .clear(1'b0),
+    .i_tdata(samples_dly_tdata), .i_tlast(1'b0), .i_tvalid(samples_dly_tvalid), .i_tready(samples_dly_tready),
+    .o_tdata(samples_conj_tdata), .o_tlast(), .o_tvalid(samples_conj_tvalid), .o_tready(samples_conj_tready));
+
+  wire [63:0] corr_tdata;
+  wire        corr_tvalid, corr_tready;
+  cmul inst_cmul_corr (
+    .clk(clk), .reset(reset), .clear(1'b0),
+    .a_tdata(samples_in_tdata[1]), .a_tlast(1'b0), .a_tvalid(samples_in_tvalid[1]), .a_tready(samples_in_tready[1]),
+    .b_tdata(samples_conj_tdata), .b_tlast(1'b0), .b_tvalid(samples_conj_tvalid), .b_tready(samples_conj_tready)
+    .o_tdata(corr_tdata), .o_tlast(), .o_tvalid(corr_tvalid), .o_tready(corr_tready));
+
+  wire [31:0] corr_rnd_tdata;
+  wire        corr_rnd_tvalid, corr_rnd_tready;
+  axi_round_and_clip_complex #(
+    .WIDTH_IN(32),
+    .WIDTH_OUT(16),
+    .CLIP_BITS(1))
+  inst_axi_round_and_clip_complex_corr (
+    .clk(clk), .reset(reset),
+    .i_tdata(corr_tdata), .i_tlast(1'b0), .i_tvalid(corr_tvalid), .i_tready(corr_tready),
+    .o_tdata(corr_rnd_tdata), .o_tlast(), .o_tvalid(corr_rnd_tvalid), .o_tready(corr_rnd_tready));
+
+  localparam CORR_WIDTH = 16+$clog2(WINDOW_LEN);
+  wire [2*CORR_WIDTH-1:0] corr_ms_tdata;
+  wire corr_ms_tvalid, corr_ms_tready;
+  moving_sum_complex #(
+    .MAX_LEN(WINDOW_LEN),
+    .WIDTH(16))
+  inst_moving_sum_corr (
+    .clk(clk), .reset(reset), .clear(1'b0),
+    .len(WINDOW_LEN),
+    .i_tdata(corr_rnd_tdata), .i_tlast(1'b0), .i_tvalid(corr_rnd_tvalid), .i_tready(corr_rnd_tready),
+    .o_tdata(corr_ms_tdata), .o_tlast(), .o_tvalid(corr_ms_tvalid), .o_tready(corr_ms_tready));
+
+  // Moving average to turn correlation plateau into peak and improve detection
+  wire [2*CORR_WIDTH-1:0] corr_ma_tdata;
+  wire corr_ma_tvalid, corr_ma_tready;
+  moving_avg_complex #(
+    .LENGTH(WINDOW_LEN/2),
+    .WIDTH(CORR_WIDTH))
+  inst_moving_avg_corr (
+    .clk(clk), .reset(reset), .clear(1'b0),
+    .i_tdata(corr_ms_tdata), .i_tlast(1'b0), .i_tvalid(corr_ms_tvalid), .i_tready(corr_ms_tready),
+    .o_tdata(corr_ma_tdata), .o_tlast(), .o_tvalid(corr_ma_tvalid), .o_tready(corr_ma_tready));
+
+  /////////////////////////////////////////////////////////
+  // Calculate power, R(d)
+  /////////////////////////////////////////////////////////
+  wire [31:0] power_tdata;
+  wire        power_tvalid, power_tready;
+  complex_to_magsq #(
+    .WIDTH(16))
+  inst_complex_to_magsq_power (
+    .clk(clk), .reset(reset), .clear(1'b0),
+    .i_tdata(samples_in_tdata[2]), .i_tlast(1'b0), .i_tvalid(samples_in_tvalid[2]), .i_tready(samples_in_tready[2]),
+    .o_tdata(power_tdata), .o_tlast(), .o_tvalid(power_tvalid), .o_tready(power_tready));
+
+  // Implicit divide by 2 to offset extended moving sum over two window lengths (see below)
+  wire [14:0] power_rnd_tdata;
+  wire        power_rnd_tvalid, power_rnd_tready;
+  axi_round #(
+    .WIDTH_IN(32),
+    .WIDTH_OUT(15))
+  inst_axi_round_power (
+    .clk(clk), .reset(reset),
+    .i_tdata(power_tdata), .i_tlast(1'b0), .i_tvalid(power_tvalid), .i_tready(power_tready),
+    .o_tdata(power_rnd_tdata), .o_tlast(), .o_tvalid(power_rnd_tvalid), .o_tready(power_rnd_tready));
+
+  // Moving sum over two window lengths to prevent false detections at beginning and end of packets
+  // This is a difference from original Schmidl Cox algorithm
+  localparam POWER_WIDTH = 15+$clog2(2*WINDOW_LEN);
+  wire [POWER_WIDTH-1:0] power_ms_tdata;
+  wire power_ms_tvalid, power_ms_tready;
+  moving_sum #(
+    .MAX_LEN(2*WINDOW_LEN),
+    .WIDTH(15))
+  inst_moving_sum_power (
+    .clk(clk), .reset(reset), .clear(1'b0),
+    .len(WINDOW_LEN),
+    .i_tdata(power_rnd_tdata), .i_tlast(1'b0), .i_tvalid(power_rnd_tvalid), .i_tready(power_rnd_tready),
+    .o_tdata(power_ms_tdata), .o_tlast(), .o_tvalid(power_ms_tvalid), .o_tready(power_ms_tready));
+
+  // Moving average to improve power estimate
+  wire [POWER_WIDTH-1:0] power_ma_tdata;
+  wire power_ma_tvalid, power_ma_tready;
+  moving_avg #(
+    .LENGTH(WINDOW_LEN/2),
+    .WIDTH(POWER_WIDTH))
+  inst_moving_avg_power (
+    .clk(clk), .reset(reset), .clear(1'b0),
+    .i_tdata(power_ms_tdata), .i_tlast(1'b0), .i_tvalid(power_ms_tvalid), .i_tready(power_ms_tready),
+    .o_tdata(power_ma_tdata), .o_tlast(), .o_tvalid(power_ma_tvalid), .o_tready(power_ma_tready));
+
+  /////////////////////////////////////////////////////////
+  // Short Preamble Peak Detection, Course Frequency Correction
+  /////////////////////////////////////////////////////////
+  wire [WIDTH-1:0] samples_out_tdata
+  wire samples_out_tlast, samples_out_tvalid, samples_out_tready;
+  wire [15:0] phase_inc_tdata
+  wire phase_inc_tlast, phase_inc_tvalid, phase_inc_tready;
+  short_preamble_detector #(
+    .WIDTH(WIDTH),
+    .CORR_WIDTH(CORR_WIDTH),
+    .POWER_WIDTH(POWER_WIDTH),
+    .SYMBOL_LEN(CYCLIC_PREFIX_LEN+SYMBOL_LEN),
+    .OFFSET(0))  // Remove short preamble
+  inst_short_preamble_detector (
+    .clk(clk), .reset(reset),
+    .num_symbols(num_symbols), .num_symbols_valid(num_symbols_valid),
+    .i_corr_tdata(corr_ma_tdata[0]), .i_corr_tvalid(corr_ma_tvalid[0]), .i_corr_tready(corr_ma_tready[0]),
+    .i_power_tdata(power_ma_tdata), .i_power_tvalid(power_ma_tvalid), .i_power_tready(power_ma_tready),
+    .i_samples_tdata(samples_in_tdata[3]), .i_samples_tlast(1'b0),
+    .i_samples_tvalid(samples_in_tvalid[3]), .i_samples_tready(samples_in_tready[3]),
+    .o_phase_tdata(phase_inc_tdata), .o_phase_tlast(phase_inc_tlast),
+    .o_phase_tvalid(phase_inc_tvalid), .o_phase_tready(phase_inc_tready),
+    .o_samples_tdata(samples_out_tdata), .o_samples_tlast(samples_out_tlast),
+    .o_samples_tvalid(samples_out_tvalid), .o_samples_tready(samples_out_tready));
+
+  wire [15:0] phase_accum_tdata;
+  wire phase_accum_tvalid, phase_accum_tready;
+  phase_accum #(
+    .WIDTH_ACCUM(16+$clog2(2*WINDOW_LEN)), // Divide by 2*WINDOW_LEN to get per sample phase offset
+    .WIDTH_IN(16),
+    .WIDTH_OUT(16))
+  inst_phase_accum (
+    .clk(clk), .reset(reset), .clear(1'b0),
+    .i_tdata(phase_inc_tdata), .i_tlast(phase_inc_tlast), .i_tvalid(phase_inc_tvalid), .i_tready(phase_inc_tready),
+    .o_tdata(phase_accum_tdata), .o_tlast(), .o_tvalid(phase_accum_tvalid), .o_tready(phase_accum_tready));
+
+  wire [31:0] samples_fc_tdata;
+  wire samples_fc_tvalid, samples_fc_tready;
+  cordic_rotator inst_cordic_rotator_coarse_freq_correction (
+    .aclk(clk), .aresetn(~reset),
+    .s_axis_phase_tdata(phase_accum_tdata), .s_axis_phase_tlast(1'b0),
+    .s_axis_phase_tvalid(phase_accum_tvalid), .s_axis_phase_tready(phase_accum_tready),
+    .s_axis_cartesian_tdata({samples_out_tdata[15:0],samples_out_tdata[31:16]}), .s_axis_cartesian_tlast(samples_out_tlast),
+    .s_axis_cartesian_tvalid(samples_out_tvalid), .s_axis_cartesian_tready(samples_out_tready),
+    .m_axis_dout_tdata({samples_fc_tdata[15:0],samples_fc_tdata[31:16]}), .m_axis_dout_tlast(samples_fc_tlast),
+    .m_axis_dout_tvalid(samples_fc_tvalid), .m_axis_dout_tready(samples_fc_tready));
+
+  /////////////////////////////////////////////////////////
+  // Long Preamble Peak Detection, Fine Timing Correction
+  /////////////////////////////////////////////////////////
+  wire [31:0] samples_lp_align_tdata;
+  wire samples_lp_align_tlast, samples_lp_align_tvalid, samples_lp_align_tready;
+  long_preamble_detector #(
+    .WIDTH(WIDTH),
+    .PREAMBLE_LEN(LONG_PREAMBLE_LEN),
+    .PREAMBLE_QUANT(LONG_PREAMBLE_QUANT),
+    .OFFSET(CP_LENGTH/2))  // Align halfway into 
+  inst_long_preamble_detector (
+    .clk(clk), .reset(reset),
+    .i_tdata(samples_fc_tdata), .i_tlast(samples_fc_tlast),
+    .i_tvalid(samples_fc_tvalid), .i_tready(samples_fc_tready),
+    .o_tdata(samples_lp_align_tdata), .o_tlast(samples_lp_align_tlast),
+    .o_tvalid(samples_lp_align_tvalid), .o_tready(samples_lp_align_tready));
+
+  cyclic_prefix_removal #(
+    .LENGTH(CYCLIC_PREFIX_LEN))
+  inst_cyclic_prefix_removal (
+    .clk(clk), .reset(reset),
+    .sof(), .eof(),
+    .i_tdata(), .i_tlast(), .i_tvalid(), .i_tready(),
+    .o_tdata(), .o_tlast(), .o_tvalid(), .o_tready());
+
+endmodule
