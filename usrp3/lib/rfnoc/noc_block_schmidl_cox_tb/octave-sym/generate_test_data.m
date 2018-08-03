f9cc53dd9b4ce14f6c96cb8e088e58bc8ce9ee40
Jonathon Pendlum <jonathon@pendlumtech.com>
2018-08-03 22:30:30 +0900

    lib: rfnoc: ofdm: Use same peak detection method in simulation as short preamble detector
    - Also minor fixes

--- usrp3/lib/rfnoc/noc_block_schmidl_cox_tb/octave-sym/generate_test_data.m
+++ usrp3/lib/rfnoc/noc_block_schmidl_cox_tb/octave-sym/generate_test_data.m
@@ -8,9 +8,10 @@ close all;
 %% User variables
 gap                     = 300;   % Number of zero samples between packets
 phase_offset            = pi/7;  % Phase offset
-freq_offset_ppm         = -100;  % TCXO frequency offset in parts per million, typical value would be +/-20
+freq_offset_ppm         = -20;   % TCXO frequency offset in parts per million, typical value would be +/-20
 gain                    = 0;     % dB
-snr                     = 10;    % dB
+snr                     = 30;    % dB
+D_threshold             = 0.8;   % Note: This value is later quantized to the nearest power of two of 1/sqrt(D_threshold)
 data_symbols_per_packet = 200;   % Number of symbols per packet (excluding preamble)
 num_packets             = 2;     % Number of packets to generate
 
@@ -47,7 +48,7 @@ long_preamble = [long_ts_t(end-31:end) long_ts_t long_ts_t];
 %% Generate packets
 preamble_offset = gap+length(short_preamble)+length(long_preamble);
 packet_length = length(short_preamble)+length(long_preamble)+data_symbols_per_packet*(cp_length+fft_length);
-test_data = zeros(1,num_packets*(gap+packet_length));
+test_data = zeros(1,num_packets*(gap+packet_length)+4*length(short_preamble));
 for i=1:num_packets
   data_symbols = generate_qpsk_symbols(randi(4, data_symbols_per_packet*length(occupied_carriers))-1);
   ofdm_data_symbols = generate_ofdm_symbols(data_symbols, fft_length, occupied_carriers, pilot_carriers, pilot_symbols, 16);
@@ -69,21 +70,36 @@ test_data_n = test_data_n  .* 10^(gain/20);
 
 %% Schmidl Cox
 [D, corr, power, phase, trigger] = schmidl_cox(test_data_n , window_length, true);
-[D_max, peak_index] = max((trigger(1:gap+length(short_preamble)) > 0).*D(1:gap+length(short_preamble)));
+% Find peak via differentiation
+D_thresh_quant = ceil(log2(1/(1-sqrt(D_threshold))));
+D_approx = (abs(corr) - (power - (1/D_thresh_quant)*power));
+D_approx = (D_approx > 0).*D_approx;
+D_approx_diff = D_approx(2:end) - D_approx(1:end-1);
+D_max = 0;
+% Find first peak
+for i=2:length(D_approx_diff)
+    if D_approx_diff(i) < 0 && D_approx_diff(i-1) > 0
+        D_max = D(i);
+        peak_index = i;
+        break;
+    end
+end
 
 if (D_max > 0)
     fprintf("Short preamble peak found at: %d\n", peak_index);
 else
     fprintf("Short preamble not detected\n");
+    return
 end
 
 % figure;
 % hold on;
 % plot(abs(corr), 'r');
 % plot(power, 'b');
-% %plot(real(test_data_n), 'k');
+% plot(real(test_data_n), 'k');
+% plot(D_approx,'m');
 % plot(D, 'g');
-% %plot(trigger, 'c');
+% plot(trigger, 'c');
 
 %% Align to start of short preamble based on detected peak from Schmidl Cox
 peak_offset = length(short_preamble);  % Peak is this far into short preamble
@@ -98,7 +114,7 @@ packet_freq_corr = add_freq_offset(packet, freq_corr);
 packet_post_sp = packet_freq_corr(length(short_preamble)-10:end); % -10 to start xcorr slightly before long preamble
 
 %% Xcorr long preamble for detection, fine timing, and frequency offset
-coeff = conj(fliplr([long_ts_t(end-31:end) long_ts_t]));
+coeff = conj(fliplr([long_ts_t(end-15:end) long_ts_t]));
 % Quantize to +/-1, reduces complex mult to 
 coeff_quant = zeros(1,length(coeff));
 for i = 1:length(coeff)
@@ -110,6 +126,7 @@ lp_xcorr_abs = abs(lp_xcorr);
 lp_xcorr_peak_indexes = sort(lp_xcorr_abs_sorted_indexs(end-1:end));  % grab two largest values, sort to make sure earlier index is first
 if (lp_xcorr_peak_indexes(2) - lp_xcorr_peak_indexes(1)) ~= window_length
   fprintf("ERROR: Did not find long preamble! Peak separation incorrect!\n")
+  return
 else
   fprintf("Long preamble peak found at: %d\n", lp_xcorr_peak_indexes(1))
 end
@@ -124,7 +141,7 @@ packet_eq_f(1,:) = equalize_ls(packet_lp_aligned, fft_length, cp_length, lp_carr
 packet_eq_f(2,:) = equalize_decision_directed(packet_lp_aligned, fft_length, cp_length, lp_carriers, occupied_carriers, guard_carriers, pilot_carriers, pilot_symbols, long_ts_f);
 
 %% Plot
-igure();
+figure();
 subplot(1,2,1);
 plot(real(packet_eq_f(2,1:200*64)));
 subplot(1,2,2);
